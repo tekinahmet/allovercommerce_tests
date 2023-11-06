5204d1344f753904365cca3b415220763d513732
justcbell <142145571+justcbell@users.noreply.github.com>
2023-11-06 21:04:48 +0200

    sibel-23.11.06

--- src/test/java/allovercommerce/tests/US14_sibel/TC01.java
+++ src/test/java/allovercommerce/tests/US14_sibel/TC01.java
@@ -1,4 +1,18 @@
 package allovercommerce.tests.US14_sibel;
 
+
+import allovercommerce.utilities.Driver;
+import org.testng.annotations.Test;
+
 public class TC01 {
-}
+
+    @Test
+    public void testCase01(){
+        Driver.getDriver().get("");
+
+    }
+
+    }
+
+
+
