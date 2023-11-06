0b3833316882bdd91663ab322dc1b713c49975e9
Ahmet Tekin <142019597+tekinahmet@users.noreply.github.com>
2023-11-06 12:19:56 +0300

    master-23.11.06-v1

--- src/test/java/allovercommerce/tests/US01_ahmet/TC01.java
+++ src/test/java/allovercommerce/tests/US01_ahmet/TC01.java
@@ -3,5 +3,7 @@ package allovercommerce.tests.US01_ahmet;
 public class TC01 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        //v3
+        System.out.println("hello world");
     }
 }
