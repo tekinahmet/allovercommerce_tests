eacdaa2c5b57fb62266a7d267957c86a9700a0d3
Ahmet Tekin <142019597+tekinahmet@users.noreply.github.com>
2023-11-16 22:05:57 +0300

    TC02_InvalidUsername.java

--- src/test/java/allovercommerce/tests/US01_ahmet/TC02_InvalidUsername.java
+++ src/test/java/allovercommerce/tests/US01_ahmet/TC02_InvalidUsername.java
@@ -2,18 +2,21 @@ package allovercommerce.tests.US01_ahmet;
 
 import allovercommerce.pages.ahmet.Ahmet_MyAccountPage;
 import allovercommerce.pages.ahmet.Ahmet_RegistrationPage;
-import allovercommerce.utilities.Driver;
-import allovercommerce.utilities.MediaUtils;
+import allovercommerce.utilities.*;
 import com.github.javafaker.Faker;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import javax.print.attribute.standard.Media;
 import java.io.IOException;
 
-public class TC02_InvalidUsername {
+public class TC01_SuccessfulRegistration {
     @Test
-    public void invalidUsername() throws IOException {
+    public void successfulRegistration() throws IOException {
+
+        ExtentReportUtils.createTestReport("successfulRegistration", "verify the steps");
+        ExtentReportUtils.pass("Starting the login test...");
+
         Ahmet_RegistrationPage ahmetRegistrationPage = new Ahmet_RegistrationPage();
         Ahmet_MyAccountPage ahmetMyAccountPage = new Ahmet_MyAccountPage();
         Faker faker = new Faker();
@@ -21,31 +24,68 @@ public class TC02_InvalidUsername {
 //        navigate to homepage
         Driver.getDriver().get("https://allovercommerce.com/");
         MediaUtils.takeScreenshotOfTheEntirePage();
+
 //        click on register button
+        JSUtils.flashElement(ahmetRegistrationPage.register);
         ahmetRegistrationPage.register.click();
         MediaUtils.takeScreenshotOfTheEntirePage();
+
 //        click on sign up
+        JSUtils.flashElement(ahmetRegistrationPage.signUp);
         ahmetRegistrationPage.signUp.click();
         MediaUtils.takeScreenshotOfTheEntirePage();
-//        enter a invalid username
-        ahmetRegistrationPage.userName.sendKeys("abc???");
+
+//        enter a valid username
+        JSUtils.flashElement(ahmetRegistrationPage.userName);
+        ahmetRegistrationPage.userName.sendKeys(faker.name().name());
         MediaUtils.takeScreenshotOfTheEntirePage();
+
 //        enter an email address
+        JSUtils.flashElement(ahmetRegistrationPage.email);
         ahmetRegistrationPage.email.sendKeys(faker.internet().emailAddress());
         MediaUtils.takeScreenshotOfTheEntirePage();
+
 //        enter a valid password
+        JSUtils.flashElement(ahmetRegistrationPage.password);
         ahmetRegistrationPage.password.sendKeys(faker.internet().password());
         MediaUtils.takeScreenshotOfTheEntirePage();
+
 //        click on "I agree to the privacy policy" box"
+        JSUtils.flashElement(ahmetRegistrationPage.policyCheckBox);
         ahmetRegistrationPage.policyCheckBox.click();
         MediaUtils.takeScreenshotOfTheEntirePage();
+
 //        click on sign up button
+        JSUtils.flashElement(ahmetRegistrationPage.signUpButton);
         ahmetRegistrationPage.signUpButton.click();
         MediaUtils.takeScreenshotOfTheEntirePage();
-//        verify that user sees "Please provide a valid email address." message
-        Assert.assertTrue(ahmetRegistrationPage.verifyUsername.isDisplayed());
+
+//        verify sign out icon is displayed on the page
+        JSUtils.flashElement(ahmetMyAccountPage.signOut);
+        Assert.assertTrue(ahmetMyAccountPage.signOut.isDisplayed());
         MediaUtils.takeScreenshotOfTheEntirePage();
+
+//        click sign out icon
+        JSUtils.flashElement(ahmetMyAccountPage.signOut);
+        ahmetMyAccountPage.signOut.click();
+        MediaUtils.takeScreenshotOfTheEntirePage();
+
+//        click on log out
+        JSUtils.flashElement(ahmetMyAccountPage.logOut);
+        ahmetMyAccountPage.logOut.click();
+        MediaUtils.takeScreenshotOfTheEntirePage();
+
+//        verify that user is on the "My Account" page
+        JSUtils.flashElement(ahmetMyAccountPage.verifyLogout);
+        Assert.assertTrue(ahmetMyAccountPage.verifyLogout.isDisplayed());
+        MediaUtils.takeScreenshotOfTheEntirePage();
+
 //        close
         Driver.getDriver().close();
+
+        ExtentReportUtils.pass("Driver is closed.... Test is completed successfully...");
+        ExtentReportUtils.flush();
+
+
     }
 }
