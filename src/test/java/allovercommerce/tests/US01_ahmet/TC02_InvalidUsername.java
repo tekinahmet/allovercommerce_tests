6db87009263530e41f83092b473945cb2edc18c6
Ahmet Tekin <142019597+tekinahmet@users.noreply.github.com>
2023-11-16 22:07:05 +0300

    TC02_InvalidUsername.java

--- src/test/java/allovercommerce/tests/US01_ahmet/TC02_InvalidUsername.java
+++ src/test/java/allovercommerce/tests/US01_ahmet/TC02_InvalidUsername.java
@@ -2,8 +2,7 @@ package allovercommerce.tests.US01_ahmet;
 
 import allovercommerce.pages.ahmet.Ahmet_MyAccountPage;
 import allovercommerce.pages.ahmet.Ahmet_RegistrationPage;
-import allovercommerce.utilities.Driver;
-import allovercommerce.utilities.MediaUtils;
+import allovercommerce.utilities.*;
 import com.github.javafaker.Faker;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -14,38 +13,72 @@ import java.io.IOException;
 public class TC02_InvalidUsername {
     @Test
     public void invalidUsername() throws IOException {
+
+        ExtentReportUtils.createTestReport("invalidUsername", "verify the steps");
+        ExtentReportUtils.pass("Starting the test...");
+
         Ahmet_RegistrationPage ahmetRegistrationPage = new Ahmet_RegistrationPage();
         Ahmet_MyAccountPage ahmetMyAccountPage = new Ahmet_MyAccountPage();
         Faker faker = new Faker();
 
 //        navigate to homepage
         Driver.getDriver().get("https://allovercommerce.com/");
+        WaitUtils.waitFor(1);
         MediaUtils.takeScreenshotOfTheEntirePage();
+
 //        click on register button
+        JSUtils.flashElement(ahmetRegistrationPage.register);
         ahmetRegistrationPage.register.click();
+        WaitUtils.waitFor(1);
         MediaUtils.takeScreenshotOfTheEntirePage();
+
 //        click on sign up
+        JSUtils.flashElement(ahmetRegistrationPage.signUp);
         ahmetRegistrationPage.signUp.click();
+        WaitUtils.waitFor(1);
         MediaUtils.takeScreenshotOfTheEntirePage();
+
 //        enter a invalid username
+        JSUtils.flashElement(ahmetRegistrationPage.userName);
         ahmetRegistrationPage.userName.sendKeys("abc???");
+        WaitUtils.waitFor(1);
         MediaUtils.takeScreenshotOfTheEntirePage();
+
 //        enter an email address
+        JSUtils.flashElement(ahmetRegistrationPage.email);
         ahmetRegistrationPage.email.sendKeys(faker.internet().emailAddress());
+        WaitUtils.waitFor(1);
         MediaUtils.takeScreenshotOfTheEntirePage();
+
 //        enter a valid password
+        JSUtils.flashElement(ahmetRegistrationPage.password);
         ahmetRegistrationPage.password.sendKeys(faker.internet().password());
+        WaitUtils.waitFor(1);
         MediaUtils.takeScreenshotOfTheEntirePage();
+
 //        click on "I agree to the privacy policy" box"
+        JSUtils.flashElement(ahmetRegistrationPage.policyCheckBox);
         ahmetRegistrationPage.policyCheckBox.click();
+        WaitUtils.waitFor(1);
         MediaUtils.takeScreenshotOfTheEntirePage();
+
 //        click on sign up button
+        JSUtils.flashElement(ahmetRegistrationPage.signUpButton);
         ahmetRegistrationPage.signUpButton.click();
+        WaitUtils.waitFor(1);
         MediaUtils.takeScreenshotOfTheEntirePage();
+
 //        verify that user sees "Please provide a valid email address." message
+        JSUtils.flashElement(ahmetRegistrationPage.verifyUsername);
         Assert.assertTrue(ahmetRegistrationPage.verifyUsername.isDisplayed());
+        WaitUtils.waitFor(1);
         MediaUtils.takeScreenshotOfTheEntirePage();
+
 //        close
         Driver.getDriver().close();
+
+        ExtentReportUtils.pass("Driver is closed.... Test is completed successfully...");
+        ExtentReportUtils.flush();
+
     }
 }
